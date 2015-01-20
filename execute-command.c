87f65189dbf86beefca658322df38fb010964135
mckennagalvin <mckennagalvin@me.com>
2015-01-19 22:43:33 -0800

    skeleton for execute-command

--- execute-command.c
+++ execute-command.c
@@ -20,12 +20,35 @@
 
 #include <error.h>
 
-/* FIXME: You may need to add #include directives, macro definitions,
-   static function definitions, etc.  */
+
+//***********************************************************************
+//                    FUNCTION DECLARATIONS
+//***********************************************************************
+
+void execute_if(command_t c);
+void execute_until(command_t c);
+void execute_while(command_t c);
+void execute_sequence(command_t c);
+void execute_pipe(command_t c);
+void execute_simple(command_t c);
+void execute_subshell(command_t c);
+void execute_io(command_t c);
+
+//***********************************************************************
+//                    AUXILARY FUNCTIONS
+//***********************************************************************
 
 int
-prepare_profiling (char const *name)
-{
+command_status (command_t c) {
+  return c->status;
+}
+
+//***********************************************************************
+//                    PREPARE_PROFILING
+//***********************************************************************
+
+int
+prepare_profiling (char const *name) {
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
@@ -33,15 +56,89 @@ prepare_profiling (char const *name)
   return -1;
 }
 
-int
-command_status (command_t c)
-{
-  return c->status;
-}
+//***********************************************************************
+//                    EXECUTE COMMAND
+//***********************************************************************
 
 void
-execute_command (command_t c, int profiling)
-{
-  /* FIXME: Replace this with your implementation, like 'prepare_profiling'.  */
-  error (1, 0, "command execution not yet implemented");
+execute_command(command_t c, int profiling) {
+  switch (c->type) {
+      
+    case IF_COMMAND:
+      execute_if(c);
+      break;
+    case UNTIL_COMMAND:
+      execute_until(c);
+      break;
+    case WHILE_COMMAND:
+      execute_while(c);
+      break;
+    case SEQUENCE_COMMAND:
+      execute_sequence(c);
+      break;
+    case PIPE_COMMAND:
+      execute_pipe(c);
+      break;
+    case SIMPLE_COMMAND:
+      execute_simple(c);
+      break;
+    case SUBSHELL_COMMAND:
+      execute_subshell(c);
+      break;
+    default:
+      error (1, 0, "command not found");
+  }
+  
+  execute_io(c);
 }
+
+
+void execute_if(command_t c) {
+  
+  int condition;
+  // TODO: evaluate command[0] as true or false, store result in condition
+  if (condition)
+    execute_command(c->u.command[1]);
+  else if (c->u.command[2] != NULL)
+    execute_command(c->u.command[2]);
+  
+}
+
+
+void execute_until(command_t c) {
+  // TODO
+}
+
+
+void execute_while(command_t c) {
+  // TODO
+}
+
+
+void execute_sequence(command_t c) {
+  // TODO
+}
+
+
+void execute_pipe(command_t c) {
+  // TODO
+}
+
+
+void execute_simple(command_t c) {
+  // TODO
+}
+
+
+void execute_subshell(command_t c) {
+  // TODO
+}
+
+
+void execute_io(command_t c) {
+  // TODO
+}
+
+
+
+
